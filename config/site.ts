777d9753784b0bdfc6a218706cb8be6d0077f161
Rouven Volk <r.volk@1xinternet.de>
2025-02-18 12:17:27 +0100

    update page url to www.helldads.org

--- config/site.ts
+++ config/site.ts
@@ -6,7 +6,7 @@ export const siteConfig = {
   name: "HellDads | Helldivers 2 Community",
   description:
     "Welcome to HellDads: The Parental Corps of Super Earth! Balancing the duty to spread managed democracy with caring for family or loved ones is no easy task. Late-night lullabies, diaper changes, or going AFK for those who need you—we understand the struggle. HellDads is for anyone juggling life’s responsibilities with a passion for Helldivers 2, we’re united by our commitment to those we care for—even if it’s after bedtime.",
-  url: "https://helldads.github.io",
+  url: "https://www.helldads.org",
   logo: "/assets/helldads-logo.svg",
   analytics: "G-GK3MK919V7",
   navItems: [
@@ -53,7 +53,7 @@ export const siteConfig = {
     },
   ],
   links: {
-    github: "https://helldads.github.io",
+    github: "https://github.com/helldads/helldads.github.io",
     reddit: "https://reddit.com/r/HellDads",
     docs: "https://reddit.com/r/HellDads/wiki",
     discord: "https://discord.gg/7rbdFMmGCz",
