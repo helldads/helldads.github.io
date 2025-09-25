d3066bce9c549e3d436804d5d08b18aee78de878
HellDads <patres.inferni@gmail.com>
2025-09-25 12:14:12 +0200

    Replace GA via GTM and update legal page accordingly.

--- config/site.ts
+++ config/site.ts
@@ -6,8 +6,7 @@ export const siteConfig = {
     "Welcome to HellDads: The Parental Corps of Super Earth! Balancing the duty to spread managed democracy with caring for family or loved ones is no easy task. Late-night lullabies, diaper changes, or going AFK for those who need you, we understand the struggle. HellDads is for anyone juggling life’s responsibilities with a passion for Helldivers 2, we’re united by our commitment to those we care for, even if it’s after bedtime.",
   url: "https://www.helldads.org",
   logo: "/assets/helldads-logo.svg",
-  analytics: "G-GK3MK919V7",
-  analyticsDebugMode: process.env.NODE_ENV == "development",
+  gtmId: "GTM-PV3X6XB4",
   navItems: [
     {
       label: "About",
